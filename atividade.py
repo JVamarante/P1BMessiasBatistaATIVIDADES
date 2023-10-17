5955af0fb1fd82fe6ebaa972cfd8eb26b53f5bf4
João Victor Monteiro Amarante <joaovamarante@gmail.com>
2023-10-16 21:18:37 -0300

    commit2

--- atividade.py
+++ atividade.py
@@ -5,4 +5,18 @@ Q = []
 for numero in range(20):
   Q.append(randint(1, 100))
 
+
+maior = -1
+menor = 101
+for i in Q:
+    if maior < i:
+        maior = i
+    if menor > i:
+        menor = i
+
+
+print('Lista de números:')
 print(Q)
+print(f'O maior valor é: {maior}')
+print(f'O menor valor é: {menor}')
+
