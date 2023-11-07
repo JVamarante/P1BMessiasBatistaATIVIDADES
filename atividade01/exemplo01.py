5ed7baad9889641a0f5ee1b603c91a2996b8f369
João Victor Monteiro Amarante <joaovamarante@gmail.com>
2023-11-06 21:43:40 -0300

    commit

--- atividade01/exemplo01.py
+++ atividade01/exemplo01.py
@@ -1,3 +1,9 @@
-with open:('arquivo.txt', 'r')
-    conteudo = file_object.read()
+with open('arquivo.txt', 'r', encoding='utfe-8') as livro:
+    conteudo = livro.read()
     print(conteudo)
+    print('------------------')
+
+with open('arquivo.txt', 'r', encoding='utf-8') as livro:
+    linhas = livro.readlines()
+    print(f'Foram impressas {len(linhas)} linhas.')
+
