8d313c26777f27ec33184c95b2ada7a044227877
João Victor <joaovamarante@gmail.com>
2023-11-20 21:13:56 -0300

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 import os
 from flask import Flask, render_template
 
@@ -28,3 +29,31 @@ def sobre():
 
 if __name__ == "__main__":
     app.run()
+
+@app.route('/glossario')
+def glossario():
+
+        glossario_de_termos = []
+
+        with open('bd_glossario.csv',
+            newline='', encoding='utf-8') as arquivo:
+            reader = csv.reader(arquivo, delimiter=';')
+            for l in reader:
+                glossario_de_termos.append(l)
+                
+            return render_template('glossario.html', glossario=glossario_de_termos)
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('adicionar_termo.html')
+
+@app.route(rule='/criar_termo', methods=['POST',])
+def criar_termo():
+    termo = request.form['termo']
+    definicao = request.form['definiçao']
+
+    with open('bd_glossario.csv', 'a', newline='', encoding='utf-8') as arquivos:
+        write = csv.write(arquivo, delimiter=';')
+        write.writerow([termo, definicao])
+
+    return redirect(url_for('ola'))
